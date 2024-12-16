c17ad7f4c081f7c52247bc87fb711ef2d79c3e9a
happ1ness <67000247+happ1nessxgod@users.noreply.github.com>
2024-12-16 22:09:28 +0300

    Create a3.cpp

---
+++ 1/a3.cpp
@@ -0,0 +1,35 @@
+#include <iostream.h>
+
+short int aaaS, bbbS, cccS; // Переменные для передачи в ASM
+int result;                 // Переменная для результата
+
+extern "C" { void Lab1S(void); }
+
+// (bbbS * cccS - 8 / aaaS) / (bbbS + 30)
+void F_ASM() {
+    Lab1S();
+    cout << "ASM result: " << result << endl;
+}
+
+void F_CPP() {
+    cout << "CPP result: ";
+    if (aaaS == 0 || bbbS + 30 == 0) { // Проверка на деление на 0
+        cout << "Error (division by zero)" << endl;
+        return;
+    }
+    result = (bbbS * cccS - 8 / aaaS) / (bbbS + 30);
+    cout << result << endl;
+}
+
+int main() {
+    cout << "Enter a: ";
+    cin >> aaaS;
+    cout << "Enter b: ";
+    cin >> bbbS;
+    cout << "Enter c: ";
+    cin >> cccS;
+
+    F_CPP();  // Вычисление на C++
+    F_ASM();  // Вычисление на ASM
+    return 0;
+}
